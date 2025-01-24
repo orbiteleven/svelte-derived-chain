4a906be91d37a5db23f6569f93ec1951b3f01085
Damon Vestervand <damon@vestervand.net>
2025-01-24 10:58:35 +0100

    more comments

--- src/lib/parent-context.svelte.ts
+++ src/lib/parent-context.svelte.ts
@@ -6,8 +6,10 @@ export class ParentContext {
 
   // This set is updated based on the values in the first set. It should be reactive.
   derivedSet = $derived.by(() => {
+    // Start out with a new plain set, otherwise we get state_unsafe_mutation as we update it
     const out = new Set<number>();
     this.set.forEach((value) => out.add(value * 2));
+    // We assume that because this is derived AND a SvelteSet, it should be reactive
     return new SvelteSet(out);
   });
 
